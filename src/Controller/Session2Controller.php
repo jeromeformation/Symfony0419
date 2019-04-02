85dae50230e5688958a4458483d4ca86714c8ff0
jerome <ambroise.jerome.formation@gmail.com>
2019-04-02 17:03:48 +0200

    Deuxieme correction Session

---
+++ src/Controller/Session2Controller.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Session2Controller extends AbstractController
+{
+    /**
+     * @Route("/session2/ajout")
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function ajoutSession(SessionInterface $session): Response
+    {
+        $ancienneValeur =$session->get('compteur', 0);
+        $session->set('compteur', $ancienneValeur + 1);
+
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/session2/panier")
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function affichageSession(SessionInterface $session): Response
+    {
+        var_dump($session->get('compteur'));
+
+
+        return $this->render('session/affichage-session.html.twig');
+    }
+}
